5c9a5fca7441da29737e9a1300486834bf791d4e
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2014-06-02 23:49:48 +0200

    Add version number to code and setup according to the major.minor.micro convention

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name='visvalingam',
-    version='0.0.1',
+    version='0.1.0',
     description='Simple implementation of the Visvalingam-Wyatt algorithm',
     author = 'Ralf Klammer',
     author_email = 'ralf.klammer@tu-dersden.de',
